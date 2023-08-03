2f515d4d178f2dc3f50d5998df2a94d9cfdd8f69
mmahias <mmahias@student.42.fr>
2023-08-03 17:03:43 +0200

    guards/decorator

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import {
     Body,
     Controller,
+    HttpCode,
+    HttpStatus,
     Post,
     Req,
   } from '@nestjs/common';
@@ -10,12 +12,13 @@ import {
   @Controller('auth')
   export class AuthController {
     constructor(private authService: AuthService) {}
-  
+
     @Post('signup')
     signup(@Body() dto: AuthDto) {
       return this.authService.signup(dto);
     }
     
+    @HttpCode(HttpStatus.OK)
     @Post('signin')
     signin(@Body() dto: AuthDto) {
       return this.authService.signin(dto);
