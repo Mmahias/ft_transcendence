eca0dfaf20edc3052d2ce5797f3711c781a05e0f
Léonard Krief <53581910+leonardkrief@users.noreply.github.com>
2023-09-09 11:00:50 +0200

    updated for game v0.5

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,27 @@
 import { ThemeProvider } from 'styled-components';
+import { AppLogo, AppHeader, AppLink, MainContentWrapper, AppWrapper } from './App.styles';
 import theme from './theme';
 import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import Game from './pages/Game';
+import Game from './pages/Game/Game';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
     return (
         <Router>
             <ThemeProvider theme={theme}>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home/>} />
-                    <Route path="/game" element={<Game />} />
-                </Routes>
-                <Footer />
+                <AppWrapper>
+                    <Navbar />
+                    <MainContentWrapper>
+                        <Routes>
+                            <Route path="/" element={<Home/>} />
+                            <Route path="/game" element={<Game />} />
+                        </Routes>
+                    </MainContentWrapper>
+                    <Footer />
+                </AppWrapper>
             </ThemeProvider>
         </Router>
     );
