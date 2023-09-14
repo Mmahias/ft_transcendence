6a06d13dc5b11935bd96c0f3ba4112a5b5490c57
laurentfrederick <laurent.frederick@icloud.com>
2023-09-14 22:08:45 +0200

    add user decorator

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from '@nestjs/passport';
+import {User} from "./decorator";
+import {UserDto} from "./dto";
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
+
+  @Get('/me')
+  async userInformation(@User('id') id: number): Promise<UserDto> {
+    const user = await this.userService.getUserById(id);
+    return {
+      id: user.id,
+      nickname: user.nickname
+    };
+  }
+
   @Get()
   async allUsers() {
     return this.userService.getAllUser();
