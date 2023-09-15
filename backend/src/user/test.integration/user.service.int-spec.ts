5c75040d33306c9322f05d0250506d92cd9670ba
laurentfrederick <laurent.frederick@icloud.com>
2023-09-15 18:32:03 +0200

    fix e2e bug

--- backend/src/user/test.integration/user.service.int-spec.ts
+++ backend/src/user/test.integration/user.service.int-spec.ts
@@ -4,11 +4,12 @@ import { UserModule } from '../user.module';
 import { UserService } from '../user.service';
 import { User } from '@prisma/client';
 import { ForbiddenException, NotFoundException } from '@nestjs/common';
-import { UserDto } from '../dto';
+import { UserDto, UserUpdateDto } from '../dto';
 
 describe('UserService integration test', () => {
   let prisma: PrismaService;
   let userService: UserService;
+  let userCreated: User;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
@@ -19,23 +20,26 @@ describe('UserService integration test', () => {
     await prisma.cleanDb();
   });
 
-  describe('create user and try to retrieve', () => {
-    let userCreated: User;
-
+  describe('create user', () => {
     it('should create a user', async () => {
-      userCreated =
-        (await userService.createUser('login42', 'login42@gmail.com')) || undefined;
+      expect.assertions(3);
+
+      userCreated = await userService.createUser('login42', 'login42@gmail.com');
       expect(userCreated.login_42).toBe('login42');
       expect(userCreated.nickname).toBe('login42');
       expect(userCreated.email).toBe('login42@gmail.com');
     });
 
     it('throw error when login is already taken', async () => {
+      expect.assertions(1);
+
       await expect(
         userService.createUser('login42', 'login43@gmail.com')
       ).rejects.toBeInstanceOf(ForbiddenException);
     });
+  });
 
+  describe('retrieve user', () => {
     it('should retrieve user by id', async () => {
       const expected: UserDto = {
         id: userCreated.id,
@@ -58,14 +62,59 @@ describe('UserService integration test', () => {
       });
     });
 
-    it('throw error if user id not found', async () => {
+    it('should retrieve user by nickname', async () => {
+      const expected: UserDto = {
+        id: userCreated.id,
+        nickname: userCreated.nickname
+      };
+
+      await userService.getUserByNickname('login42').then((userDto) => {
+        expect(userDto).toStrictEqual(expected);
+      });
+    });
+
+    it('throw error if user id is not found', async () => {
       await expect(userService.getUserById(99)).rejects.toBeInstanceOf(NotFoundException);
     });
 
-    it('throw error if login not found', async () => {
+    it('throw error if login is not found', async () => {
       await expect(userService.getUserByLogin42('login_unknown')).rejects.toBeInstanceOf(
         NotFoundException
       );
+    });
+
+    it('throw error if nickname is not found', async () => {
+      await expect(
+        userService.getUserByNickname('nickname_unknown')
+      ).rejects.toBeInstanceOf(NotFoundException);
+    });
+  });
+
+  describe('update a user', () => {
+    it('should update user', async () => {
+      expect.assertions(1);
+      await userService
+        .updateUser(userCreated.id, new UserUpdateDto('NEW_NICKNAME'))
+        .then((userUpdated) => {
+          expect(userUpdated.nickname).toBe('NEW_NICKNAME');
+        });
+    });
+
+    it('throw an error when nickname is already taken', async () => {
+      expect.assertions(1);
+
+      const { id } = await prisma.user.create({
+        data: {
+          login_42: 'login43',
+          nickname: 'login43',
+          email: 'login43@mail',
+          avatar: 'default_path'
+        }
+      });
+
+      await expect(
+        userService.updateUser(id, new UserUpdateDto('NEW_NICKNAME'))
+      ).rejects.toBeInstanceOf(ForbiddenException);
     });
   });
 });
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
-import { UserDto } from './dto';
+import { UserDto, UserUpdateDto } from './dto';
 
 @Injectable()
 export class UserService {
@@ -15,47 +15,6 @@ export class UserService {
 
   async getAllUser() {
     return this.prisma.user.findMany();
-  }
-
-  async getUserByLogin42(login: string) {
-    return this.prisma.user
-      .findUniqueOrThrow({
-        where: {
-          login_42: login
-        }
-      })
-      .then((user) => {
-        const userDto: UserDto = {
-          id: user.id,
-          nickname: user.nickname
-        };
-        return userDto;
-      })
-      .catch((error) => {
-        this.logger.error(error.message);
-        if (error instanceof PrismaClientKnownRequestError) {
-          throw new NotFoundException(`Login [${login}] is not found`);
-        }
-        throw error;
-      });
-  }
-
-  async createUser(login: string, email: string) {
-    return this.prisma.user
-      .create({
-        data: {
-          login_42: login,
-          nickname: login,
-          email: email
-        }
-      })
-      .catch((error) => {
-        if (error instanceof PrismaClientKnownRequestError) {
-          if (error.code === 'P2002') {
-            throw new ForbiddenException(`Login [${login}] is already taken`);
-          }
-        }
-      });
   }
 
   async getUserById(id: number) {
@@ -73,9 +32,101 @@ export class UserService {
         return userDto;
       })
       .catch((error) => {
+        if (error instanceof PrismaClientKnownRequestError) {
+          this.logger.log(`User id [${id}] is not found`);
+          throw new NotFoundException(`User id [${id}] is not found`);
+        }
+        throw error;
+      });
+  }
+
+  async getUserByNickname(nickname: string) {
+    return this.prisma.user
+      .findUniqueOrThrow({
+        where: {
+          nickname
+        }
+      })
+      .then((user) => {
+        const userDto: UserDto = {
+          id: user.id,
+          nickname: user.nickname
+        };
+        return userDto;
+      })
+      .catch((error) => {
         this.logger.error(error.message);
         if (error instanceof PrismaClientKnownRequestError) {
-          throw new NotFoundException(`User id [${id}] is not found`);
+          throw new NotFoundException(`User with nickname [${nickname}] is not found`);
+        }
+        throw error;
+      });
+  }
+
+  async getUserByLogin42(login: string) {
+    return this.prisma.user
+      .findUniqueOrThrow({
+        where: {
+          login_42: login
+        }
+      })
+      .then((user) => {
+        const userDto: UserDto = {
+          id: user.id,
+          nickname: user.nickname
+        };
+        return userDto;
+      })
+      .catch((error) => {
+        if (error instanceof PrismaClientKnownRequestError) {
+          this.logger.log(`Login [${login}] is not found`);
+          throw new NotFoundException(`Login [${login}] is not found`);
+        }
+        throw error;
+      });
+  }
+
+  async createUser(login: string, email: string) {
+    return this.prisma.user
+      .create({
+        data: {
+          login_42: login,
+          nickname: login,
+          email: email,
+          avatar: 'default_path_avatar'
+        }
+      })
+      .catch((error) => {
+        if (error instanceof PrismaClientKnownRequestError) {
+          if (error.code === 'P2002') {
+            throw new ForbiddenException(`Login [${login}] is already taken`);
+          }
+        }
+        throw error;
+      });
+  }
+
+  async updateUser(id: number, fieldsToUpdate: UserUpdateDto) {
+    return this.prisma.user
+      .update({
+        where: { id },
+        data: {
+          nickname: fieldsToUpdate.nickname
+        }
+      })
+      .then((userUpdated) => {
+        return {
+          id: userUpdated.id,
+          nickname: userUpdated.nickname
+        };
+      })
+      .catch((error) => {
+        if (error instanceof PrismaClientKnownRequestError) {
+          if (error.code === 'P2002') {
+            throw new ForbiddenException(
+              `Nickname [${fieldsToUpdate.nickname}] is already taken`
+            );
+          }
         }
         throw error;
       });
