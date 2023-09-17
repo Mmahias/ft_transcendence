9f0b40579203284e578cf980af891ea67a601fcb
mmahias <mmahias@student.42.fr>
2023-09-17 16:43:46 +0200

    add query/axios

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './styles.css';
+import { useQuery } from 'react-query';
 import { Link as RouterLink } from 'react-router-dom';
+import axios from 'axios';
 
 const Home: React.FC = () => {
     return (
@@ -9,7 +11,7 @@ const Home: React.FC = () => {
             <h1> <span> FT_TRANSCENDENCE </span> </h1>
           </p>
         <div className="button-container">
-        <RouterLink to="/"><button className="button-52" role="button">42_Login</button></RouterLink>
+        <button className="button-52" role="button" >42_Login</button>
         <RouterLink to="/"><button className="button-52" role="button">Sign_In</button></RouterLink>
         <RouterLink to="/"><button className="button-52" role="button">Sign_Up</button></RouterLink>
         </div>
