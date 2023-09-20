e78b1383ae6de0f0b7d6a1f594d3a2ed4c8c00ce
Leonard KRIEF <lkrief@made-f0Dr7s11.clusters.42paris.fr>
2023-09-20 22:32:39 +0200

    vite service commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,7 @@
-import * as path from "path";
 import { defineConfig } from 'vite'
-// import react from "@vitejs/plugin-react"
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
-  resolve: {
-    alias: {
-      components: `${path.resolve(__dirname, "./src/components/")}`,
-      pages: path.resolve(__dirname, "./src/pages"),
-      assets:  path.resolve(__dirname, "./src/assets")
-    },
-  },
-  server: {
-    watch: {
-        usePolling: true,
-    },
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 3000, // you can replace this port with any port
-    },
-});
+  plugins: [react()],
+})
