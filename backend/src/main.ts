ad7f8bf6d9ea6628b24a3eba2b0d923a125a2a7a
laurentfrederick <laurent.frederick@icloud.com>
2023-09-28 19:56:47 +0200

    allow validations

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   //   credentials: true,
   // });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3030);
   console.log('Server running on http://localhost:3030');
 }
